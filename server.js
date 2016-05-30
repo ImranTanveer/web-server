37cf417df9fb8325b09088dee507007ecb2aefd6
Imran Tanveer <imran.tanveer@seecs.edu.pk>
2016-05-30 15:34:38 +0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+/*app.get('/',function(req,res){
+
+	res.send('Hello Express');
+});*/
+
+var middleWare = {
+
+	requireAuthentication : function(req,res,next){
+		console.log('Private route hit')
+		next();
+	},
+
+	logger : function(req,res,next){
+		console.log ('Request '+req.originalUrl+' '+req.method+' on '+ new Date().toString());
+		next();
+	}
+};
+
+app.use(middleWare.logger);
+
+
+app.get('/about',middleWare.requireAuthentication,function(req,res){
+
+	res.send('About Express');
+});
+
+//app.use(middleWare.requireAuthentication);
+
+app.use(express.static(__dirname+'/public'));
+app.listen(port,function(){
+	console.log('Server got started using port'+port);
+});
