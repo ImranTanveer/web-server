28a5b5a98129990fe12bdc29b5d01bc87d5402f9
Imran Tanveer <imran.tanveer@seecs.edu.pk>
2016-05-30 17:29:58 +0500

    removed commented code

--- server.js
+++ server.js
@@ -8,18 +8,6 @@ var port = 3000;
 });*/
 
 var middleWare = require('./middleware.js');
-/*var middleWare = {
-
-	requireAuthentication : function(req,res,next){
-		console.log('Private route hit')
-		next();
-	},
-
-	logger : function(req,res,next){
-		console.log ('Request '+req.originalUrl+' '+req.method+' on '+ new Date().toString());
-		next();
-	}
-};*/
 
 app.use(middleWare.logger);
 
